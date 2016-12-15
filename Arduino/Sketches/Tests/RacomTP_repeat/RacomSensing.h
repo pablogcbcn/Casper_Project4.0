f21373adcff85966bb62d8c10e5e7dffb3a69af0
ThomHPL <thomas.herpoel@gmail.com>
2016-12-15 13:37:29 +0100

    reorder arduino folders

---
+++ Arduino/Sketches/Tests/RacomTP_repeat/RacomSensing.h
@@ -0,0 +1,5 @@
+
+#define UART_INTERFACE
+
+#include <RACOM.h>
+
---
+++ Arduino/Sketches/Tests/RacomTP_repeat/RacomTP_repeat.ino
@@ -0,0 +1,25 @@
+#include "RacomSensing.h" // needed to choose the used interface (see RacomSensing.h)
+#include <RACOM.h> // include RACOM lib
+
+void setup() {
+  Racom.begin(); // begin Transport layer
+}
+
+void loop() {
+  // first check if a packet is available
+  if(Racom.available() == 1) {
+    //read the size of the available data
+    uint16_t dSize = Racom.dSize(); 
+    // allocate memory for the packet
+    uint8_t *data = malloc(dSize);
+    // read the packet and test if the reading suceeded
+    if(Racom.read(&dSize,data)==1){
+      //send back the exact same packet for testing purposes
+      Racom.send(dSize,data);
+    }
+    //free the packet memory
+    free(data);
+    data = NULL;
+  }
+
+}
