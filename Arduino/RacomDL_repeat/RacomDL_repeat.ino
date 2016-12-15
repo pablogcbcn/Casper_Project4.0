0589395df6cce1d1f842726adb90d09d563d9499
ThomHPL <thomas.herpoel@gmail.com>
2016-12-15 13:31:28 +0100

    Comment DL repeat test

--- Arduino/RacomDL_repeat/RacomDL_repeat.ino
+++ Arduino/RacomDL_repeat/RacomDL_repeat.ino
@@ -1,21 +1,23 @@
-#include "RacomSensing.h"
-#include <RACOM.h>
+#include "RacomSensing.h" // needed to choose the used interface (see RacomSensing.h)
+#include <RACOM.h> // include RACOM lib
 
 void setup() {
-  RacomDL.begin();
-
-//  uint8_t packet[] = {0,1,2,3,4,5,6,7};
-//  RacomDL.send(sizeof(packet),packet);
-//  
+  RacomDL.begin(); // begin Data Layer
 }
 
 void loop() {
+  // first check if a packet is available
   if(RacomDL.available() == 1) {
+    //read the size of the available packet
     uint8_t pSize = RacomDL.pSize(); 
+    // allocate memory for the packet
     uint8_t *packet = malloc(pSize);
+    // read the packet and test if the reading suceeded
     if(RacomDL.read(&pSize,packet)==1){
+      //send back the exact same packet for testing purposes
       RacomDL.send(pSize,packet);
     }
+    //free the packet memory
     free(packet);
     packet = NULL;
   }
